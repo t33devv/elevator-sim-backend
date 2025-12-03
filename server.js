9cfe3581e360cd60d9adae2657c49bf1407a2961
Tommy Zhou <tommyzhou03@gmail.com>
2025-12-03 13:01:40 +0800

    pingpong calls

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
 setupSocketHandlers(io);
 
 server.listen(3000, () => {
